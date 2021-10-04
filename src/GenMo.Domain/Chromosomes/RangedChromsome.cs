476838da760dce731513bfd1e50a13ed42d6ad06
mikeg <mikegeor@outlook.com.gr>
2021-10-04 21:24:20 +0300

    Added Basic Chromosome Logic
    Started laying out the chromosomes and their different types.

---
+++ src/GenMo.Domain/Chromosomes/RangedChromsome.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GenMo.Domain.Common.Utils;
+
+namespace GenMo.Domain.Chromosomes
+{
+    public struct RangedChromsome
+    {
+        public IList<int> Genes { get; private init; }
+
+        public RangedChromsome(int chromosomeLength) =>
+            Genes = Enumerable.Range(0, chromosomeLength)
+                .ToArray()
+                .Shuffle();
+
+
+        public RangedChromsome(IList<int> genome)
+            => Genes = genome;
+
+        public override string ToString() =>
+            string.Join(", ", Genes);
+    }
+}
