4d0f2b0bc44c98ef541faca1ec507287b33340ea
mikeg <mikegeor@outlook.com.gr>
2021-10-04 21:29:45 +0300

    Added Shuffle Extension method on IList
    Wrote an extension method for shuffling an IList and returning it.

---
+++ src/GenMo.Domain/Common/Utils/ArrayUtil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenMo.Domain.Common.Utils
+{
+    public static class ArrayUtil
+    {
+        public static IList<T> Shuffle<T>(this IList<T> array)
+        {
+            var arrayLength = array.Count;
+
+            if (arrayLength == 0)
+            {
+                return array;
+            }
+            else
+            {
+                while (arrayLength > 1)
+                {
+                    var swapPosition = RandomUtil.Next(0, arrayLength--);
+                    var tempValue = array[arrayLength];
+                    array[arrayLength] = array[swapPosition];
+                    array[swapPosition] = tempValue;
+                }
+                return array;
+            }
+        }
+    }
+}
